c1c26da7e646781b0ceb9b7bd677ac762140f2a9
PanchalNihar <nihar2624@gmail.com>
2025-08-03 11:02:51 +0530

    Refactor Mood Music App: Implement MusicRecommendationComponent with routing, styles, and functionality
    - Removed previous app.component structure and replaced it with a router outlet for better routing management.
    - Created MusicRecommendationComponent to handle music recommendations based on user mood.
    - Added HTML structure and CSS styles for the new component, including responsive design and animations.
    - Implemented file upload and text input functionalities for mood detection.
    - Integrated theme toggle functionality for user preference.
    - Set up routing for the application to navigate between components.
    - Added unit tests for the new MusicRecommendationComponent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import(
+        './components/music-recommendation/music-recommendation.component'
+      ).then((m) => m.MusicRecommendationComponent),
+  },
+  {
+    path: 'music-recommendation',
+    loadComponent: () =>
+      import(
+        './components/music-recommendation/music-recommendation.component'
+      ).then((m) => m.MusicRecommendationComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
