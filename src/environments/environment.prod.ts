0116c1d877f55813c82e1ee1a275588b84221c84
PanchalNihar <nihar2624@gmail.com>
2025-08-24 22:04:21 +0530

    add env files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: "https://your-backend.onrender.com",
+  client_id: '61367993371-uo7te2rdier6c48o1002tr1gbhri5a91.apps.googleusercontent.com'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://127.0.0.1:8000",
+  client_id: '61367993371-uo7te2rdier6c48o1002tr1gbhri5a91.apps.googleusercontent.com'
+};
