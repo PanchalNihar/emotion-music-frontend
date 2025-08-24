1b04580f9723079f47729338e3066aa5814f62aa
PanchalNihar <nihar2624@gmail.com>
2025-08-24 22:58:34 +0530

    update production api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://your-backend.onrender.com",
+  apiUrl: "https://tuneify-backend-br4d.onrender.com",
   client_id: '61367993371-uo7te2rdier6c48o1002tr1gbhri5a91.apps.googleusercontent.com'
 };
